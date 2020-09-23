ff8d8f509de7c5c7038cb723b82e6a17197d4936
yancy-git <593704652@qq.com>
2020-09-23 12:52:35 +0800

    9.23提交测试

---
+++ testpro/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+ <div>黑恶嘿嘿</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+   
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
